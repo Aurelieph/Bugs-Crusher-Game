bffbb9870f85e7c6f54b559ca70a7792ed9c0b2a
Naing Grace <grace.naing@hes-so.ch>
2024-12-19 14:21:06 +0100

    settings of box

--- src/Base.scala
+++ src/Base.scala
@@ -1,4 +1,16 @@
-object Base {
+import hevs.graphics.FunGraphics
+
+object Base extends App{
+  val display: FunGraphics = new FunGraphics(600, 800, "Our Super Game")
+
+  val box: Array[Array[Int]] = Array.ofDim(9,9)
+  // val color: List[Int]
+
+  for (i <- box.indices){
+    for (j <- box(i).indices){
+      box(i)(j) = (Math.random()*5).toInt
+    }
+  }
 
 
 }
