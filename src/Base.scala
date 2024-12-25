05e7ec32795611e255c617779604268058527b58
Aurelie <42074299+Aurelieph@users.noreply.github.com>
2024-12-25 23:46:08 +0100

    Get positions of same numbers

--- src/Base.scala
+++ src/Base.scala
@@ -22,10 +22,10 @@ object Base extends App {
 
   val margin: Int = 40
   val caseWidth: Int = (width - margin * 2) / nbOfElement
-  println(caseWidth)
   val boxWidth: Int = caseWidth * nbOfElement
   val possibilities: Array[Int] = Array(1, 2, 3, 4, 5)
-  val fontSize: Int = 8
+  val fontSize: Int = 14
+  val noPositionValue: Int = -1
   var mySelection = new Selections()
 
   def initializeElements(arr: Array[Int]): Unit = {
@@ -55,34 +55,64 @@ object Base extends App {
 
     //TODO make sure we will manage the deselection once the resolution of the action will occur.
     def update(position: Position): Unit = {
-      if (firstSelection.x == -1 || firstSelection.y == -1) {
+      if (firstSelection.x == noPositionValue || firstSelection.y == noPositionValue) {
         secondSelection = new Position
         firstSelection = position
       }
       else {
         secondSelection = position
       }
-      //println(firstSelection.x, firstSelection.y, secondSelection.x, secondSelection.y)
-    }
-
-    def getValueElement(arr: Array[Array[Int]], position: Position): Int = {
-      arr(position.x)(position.y)
     }
 
     //TODO update
-    def getHorizontalNeighbours(arr: Array[Array[Int]]): Int = {
-      val leftLimit: Int = Math.max(firstSelection.x - 2, 0)
-      val rightLimit: Int = Math.min(firstSelection.x + 2, width)
-
-      for (i <- leftLimit to rightLimit) {
+    def getHorizontalNeighbours(arr: Array[Array[Int]], position: Position): Array[Position] = {
+      val currentValue: Int = arr(position.x)(position.y)
+      val leftNeighboursCoordinates: Array[Position] = new Array(2)
+      val rightNeighboursCoordinates: Array[Position] = new Array(2)
+      var sameLeftValue: Boolean = true
+      var sameRightValue: Boolean = true
+      for (i <- 0 to 1) {
+        val leftPosition: Int = position.x - i - 1
+        if (leftPosition >= 0 && arr(leftPosition)(position.y) == currentValue && sameLeftValue) {
+          leftNeighboursCoordinates(i) = new Position(leftPosition, position.y)
+        }
+        else {
+          leftNeighboursCoordinates(i) = new Position()
+          sameLeftValue = false
+        }
+        val rightPosition: Int = position.x + i + 1
+        if (rightPosition <= nbOfElement - 1 && arr(rightPosition)(position.y) == currentValue && sameRightValue) {
+          rightNeighboursCoordinates(i) = new Position(rightPosition, position.y)
+        }
+        else {
+          rightNeighboursCoordinates(i) = new Position()
+          sameRightValue = false
+        }
 
       }
 
-      0
+      val tempArray: Array[Position] = new Array(4)
+      var count: Int = 0
+      for (i <- rightNeighboursCoordinates.indices) {
+        if (rightNeighboursCoordinates(i).x != noPositionValue) {
+          tempArray(count) = rightNeighboursCoordinates(i)
+          count += 1
+        }
+        if (leftNeighboursCoordinates(i).x != noPositionValue) {
+          tempArray(count) = leftNeighboursCoordinates(i)
+          count += 1
+        }
+      }
+      val (coordinatesResult, empty) = tempArray.splitAt(count)
+      for (i <- coordinatesResult.indices) {
+        println(coordinatesResult(i).x, coordinatesResult(i).y)
+
+      }
+
+      coordinatesResult
     }
 
   }
-
 
   initializeElements(possibilities)
 
@@ -97,6 +127,10 @@ object Base extends App {
       val caseNumberY = (clickY - margin) / caseWidth
 
       mySelection.update(new Position(caseNumberX, caseNumberY))
+      var sameNeighbours: Array[Position] = mySelection.getHorizontalNeighbours(box, new Position(caseNumberX, caseNumberY))
+      for (i <- sameNeighbours) {
+        println("neighoubours", i.x, i.y)
+      }
 
       val cellX: Int = caseNumberX * caseWidth + margin
       val cellY: Int = caseNumberY * caseWidth + margin - fontSize
