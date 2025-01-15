6fb74e6acf0c360f90ae133ab288be883792167d
Aurelie <42074299+Aurelieph@users.noreply.github.com>
2025-01-15 15:55:38 +0100

    Update score

--- src/Scoring.scala
+++ src/Scoring.scala
@@ -1,11 +1,12 @@
 import hevs.graphics.utils.GraphicsBitmap
 
 // Class to store the score of the current level
-class Scoring(var level:Int) {
-  var score:Int = 0
-  var goal:Int = 1000
-  var movesLeft = 10 - level -1
+class Scoring(var level: Int) {
+  var score: Int = 0
+  var goal: Int = 1000
+  var movesLeft = 10 - level + 1
   val lastLevel = 5
+
   def decreaseMove(): Unit = {
     movesLeft -= 1
   }
@@ -17,22 +18,23 @@ class Scoring(var level:Int) {
   def goalReached(): Boolean = {
     score >= goal
   }
+
   def isVictory(): Boolean = {
-    level > lastLevel || (level==lastLevel && isLevelFinished())
+    level > lastLevel || (level == lastLevel && isLevelFinished())
   }
 
   // Show different images based on the state of the game
   def endMessage(): GraphicsBitmap = {
-      val levelup = new GraphicsBitmap("/res/level.png")
-      val gameover = new GraphicsBitmap("/res/gameover.png")
-      val victory = new GraphicsBitmap("/res/win.png")
+    val levelup = new GraphicsBitmap("/res/level.png")
+    val gameover = new GraphicsBitmap("/res/gameover.png")
+    val victory = new GraphicsBitmap("/res/win.png")
 
-      if (isVictory()) {
-        return victory
-      }
-      if (isLevelFinished && !goalReached()){
-        return gameover
-      }
+    if (isVictory()) {
+      return victory
+    }
+    if (isLevelFinished && !goalReached()) {
+      return gameover
+    }
     levelup
   }
 
